6dd19c9d5c8d95ec1eefa8a57613f931a010543a
Juan Pablo Gonzales <juampi7237@gmail.com>
2023-10-11 08:59:27 -0400

    Primer caso de prueba

--- cypress/e2e/pages/login/login.methods.js
+++ cypress/e2e/pages/login/login.methods.js
@@ -4,13 +4,13 @@ import { LoginElements } from "./login.elements";
 
 export class LoginMethods {
   static insertUsername(username) {
-    LoginElements.textboxes.username.type(username);
-    //LoginElements.textboxes.username.invoke("val", username);
+    //LoginElements.textboxes.username.type(username);
+    LoginElements.textboxes.username.invoke("val", username);
   }
 
   static insertPassword(password) {
-    LoginElements.textboxes.password.type(password);
-    //LoginElements.textboxes.password.invoke("val", password);
+    //LoginElements.textboxes.password.type(password);
+    LoginElements.textboxes.password.invoke("val", password);
   }
 
   static clickOnLoginButton() {
