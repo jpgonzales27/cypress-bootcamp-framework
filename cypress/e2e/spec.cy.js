1139ee482cace16f6abc9453f7303932089d4f65
Juan Pablo Gonzales <juampi7237@gmail.com>
2023-10-13 15:28:24 -0400

    ejemplo de tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,29 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+import { HomeMethods } from "./pages/Home/home.methods";
+import { CartMethods } from "./pages/cart/cart.methods";
+import { CommonPageElements } from "./pages/common-page/common-page.elements";
+import { CommonPageMethods } from "./pages/common-page/common-page.methods";
+import { LoginMethods } from "./pages/login/login.methods";
+import { Logger } from "./util/logger";
+
+describe.skip("template spec", () => {
+  it("passes", () => {
+    const username = "random01";
+    const password = "random01";
+
+    //Logger.stepNumber(1);
+    Logger.step("Navegar a la demoblaze page");
+    cy.visit("https://www.demoblaze.com/index.html");
+
+    //Logger.stepNumber(2);
+    Logger.step("Click on 'Login' link");
+    CommonPageMethods.clickOnLoginOption();
+
+    //Logger.stepNumber(3);
+    Logger.step(`Login with credentials: ${username}/${password}}`);
+    LoginMethods.login(username, password);
+
+    //Logger.stepNumber(4);
+    Logger.verification(`The home should show: Welcome ${username}`);
+    cy.get("a#nameofuser").should("contain.text", username);
+  });
+});
